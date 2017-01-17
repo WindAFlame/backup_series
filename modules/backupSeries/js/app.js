08bdbbb89b16743d907fb18984e4b6bf1e4223af
WindAFlame <stigced@gmail.com>
2017-01-17 14:23:37 +0100

    Add offline version of the project. Playlist Version.

---
+++ modules/backupSeries/js/app.js
@@ -0,0 +1,9 @@
+var app = angular.module('myApp', 
+    ["ngSanitize",
+    "com.2fdevs.videogular",
+    "com.2fdevs.videogular.plugins.controls",
+    "com.2fdevs.videogular.plugins.overlayplay",
+    "com.2fdevs.videogular.plugins.poster",
+    "com.2fdevs.videogular.plugins.buffering"
+    ]
+);
